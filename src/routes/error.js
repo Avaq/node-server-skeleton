8ac029b3c0040b7a718ed801bb464c7824c0d39d
Avaq <aldwin.vlasblom@gmail.com>
2015-11-27 11:17:22 +0100

    Add better error handling and logging

--- src/routes/error.js
+++ src/routes/error.js
@@ -1,5 +1,9 @@
 'use strict';
 
+import createError from 'http-errors';
+import {line, warn} from '../util/common';
+import {log} from 'util';
+
 const normalizeError = err => {
   const {status, message, stack} = err;
   return Object.assign({
@@ -10,6 +14,23 @@ const normalizeError = err => {
 };
 
 export default router => {
+
+  //Stacks that reach to here are 404.
+  router.all('*', (req, res, next) => next(createError(404, line `
+    There's nothing at ${req.method.toUpperCase()} ${req.path}
+  `)));
+
+  //Log errors.
+  router.use((err, req, res, next) => {
+
+    if(!err.status || err.status >= 500){
+      return next(warn(err));
+    }
+
+    log(`${req.name}: [${err.status}] ${err.message}`);
+    next(err);
+
+  });
 
   //Error responses.
   /*eslint no-unused-vars:0*/
