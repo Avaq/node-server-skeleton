2d1307548b172f84e08fe153fdfbbbd9319ae27d
Avaq <aldwin.vlasblom@gmail.com>
2015-12-02 11:33:17 +0100

    Add HTTPS support

--- src/index.js
+++ src/index.js
@@ -4,13 +4,40 @@ import server from './app';
 import config from 'config';
 import executiveUser from 'executive-user';
 import {log} from 'util';
+import https from 'https';
+import http from 'http';
 
-const connection = server.listen(
-  config.get('server.port'),
-  config.get('server.host'),
-  () => {
-    const addr = connection.address();
-    executiveUser(config.get('process.uid'), config.get('process.gid'));
-    log('[SERVER] Listening on %s:%s', addr.address, addr.port);
-  }
+let connectingServers = 0;
+
+const setExecutiveUser = () => --connectingServers > 0 || executiveUser(
+  config.get('process.uid'),
+  config.get('process.gid')
 );
+
+if(config.get('server.http.enabled')){
+  connectingServers += 1;
+  const connection = http.createServer(server).listen(
+    config.get('server.http.port'),
+    config.get('server.http.host'),
+    () => {
+      const addr = connection.address();
+      log('HTTP Server listening on %s:%s', addr.address, addr.port);
+      setExecutiveUser();
+    }
+  )
+}
+
+if(config.get('server.https.enabled')){
+  connectingServers += 1;
+  const key = (config.get('server.https.key'));
+  const cert = (config.get('server.https.cert'));
+  const connection = https.createServer({key, cert}, server).listen(
+    config.get('server.https.port'),
+    config.get('server.https.host'),
+    () => {
+      const addr = connection.address();
+      log('HTTPS Server listening on %s:%s', addr.address, addr.port);
+      setExecutiveUser();
+    }
+  )
+}
