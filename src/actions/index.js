eed7f3aefbe57dcc80b4bd3fa9a85c27bc82d7a2
Avaq <aldwin.vlasblom@gmail.com>
2016-01-13 21:29:40 +0100

    Update default index file

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,18 @@
 'use strict';
 
+import meta from '../../package';
+import {K, pipe} from 'sanctuary';
 import {Future} from 'ramda-fantasy';
+import {evolve, map, unary} from 'ramda';
 
-export default () => Future.of('Hello world!');
+export default unary(pipe([
+  K(Future.of({
+    name: meta.name,
+    version: meta.version,
+    machine: process.env.HOSTNAME || process.env.HOST || '<unknown>',
+    uptime: '<unknown>'
+  })),
+  map(evolve({
+    uptime: process.uptime
+  }))
+]));
