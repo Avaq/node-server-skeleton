f7d935d672b6066f5344263e89ea98bdb4d10939
Avaq <aldwin.vlasblom@gmail.com>
2016-12-19 10:08:45 +0100

    Fix incorrect type signature

--- src/bootstrap/config.js
+++ src/bootstrap/config.js
@@ -5,7 +5,7 @@ const config = require('config');
 const {putService} = require('../util/service');
 const Future = require('fluture');
 
-//    loadConfig :: String -> Middleware a Error Any
+//    loadConfig :: String -> Future Error Any
 const loadConfig = prop => Future.try(_ => config.get(prop));
 
 //This bootstrapper registers a config service which loads application settings.
