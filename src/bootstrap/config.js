8ff2562a0a35aca8b154f9a846896939c94244f0
Avaq <aldwin.vlasblom@gmail.com>
2017-01-17 11:27:34 +0100

    Abort bootstrap if environment is not set
    This because by default, node-config uses "development"
    in the absence of an environment. This cause issues in
    production because of lazy configuration.

--- src/bootstrap/config.js
+++ src/bootstrap/config.js
@@ -3,7 +3,7 @@
 const config = require('config');
 const {putService} = require('../util/service');
 const Future = require('fluture');
-const {App} = require('momi');
+const {App, Middleware} = require('momi');
 const log = require('../util/log');
 
 //    loadConfig :: String -> Future Error Any
@@ -12,11 +12,11 @@ const loadConfig = prop => Future.try(_ => config.get(prop));
 //This bootstrapper registers a config service which loads application settings.
 module.exports = App.do(function*(next) {
 
-  if(process.env.NODE_ENV) {
-    log.info(`Running with ${process.env.NODE_ENV} environment`);
-  } else {
-    log.warn('Running without environment');
+  if(!process.env.NODE_ENV) {
+    yield Middleware.lift(Future.reject(new Error('NODE_ENV is not set')));
   }
+
+  log.info(`Using "${process.env.NODE_ENV}" configurations`);
 
   yield putService('config', loadConfig);
 
