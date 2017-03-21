058108bb9e3150cb8864ea4652d40f6e6763aaa4
Aldwin Vlasblom <aldwin.vlasblom@gmail.com>
2017-03-21 14:26:04 +0100

    Rename exports to E

--- src/env.js
+++ src/env.js
@@ -9,41 +9,43 @@ const F_DOCS = 'https://github.com/Avaq/Fluture';
 const COMPARE_DOCS = 'https://developer.mozilla.org/en-US/docs/Web/JavaScript/'
   + 'Reference/Global_Objects/Array/sort#Description';
 
+const E = module.exports;
+
 const last = xs => xs[xs.length - 1];
-const is = (T, x) => test(exports.env, T, x);
+const is = (T, x) => test(E.env, T, x);
 
-env.forEach(type => exports[`$${last(type.name.split('/'))}`] = type);
+env.forEach(type => E[`$${last(type.name.split('/'))}`] = type);
 
-exports.$a = TypeVariable('a');
-exports.$b = TypeVariable('b');
-exports.$c = TypeVariable('c');
-exports.$d = TypeVariable('d');
-exports.$e = TypeVariable('e');
-exports.$f = UnaryTypeVariable('f');
-exports.$g = TypeVariable('g');
-exports.$m = UnaryTypeVariable('m');
+E.$a = TypeVariable('a');
+E.$b = TypeVariable('b');
+E.$c = TypeVariable('c');
+E.$d = TypeVariable('d');
+E.$e = TypeVariable('e');
+E.$f = UnaryTypeVariable('f');
+E.$g = TypeVariable('g');
+E.$m = UnaryTypeVariable('m');
 
-exports.$Either = EitherType;
-exports.$Maybe = MaybeType;
-exports.$Array = $.Array;
-exports.$Function = $.Function;
-exports.$Pair = $.Pair;
+E.$Either = EitherType;
+E.$Maybe = MaybeType;
+E.$Array = $.Array;
+E.$Function = $.Function;
+E.$Pair = $.Pair;
 
-exports.$Compare = NullaryType('Compare', COMPARE_DOCS, x => x === -1 || x === 0 || x === 1);
+E.$Compare = NullaryType('Compare', COMPARE_DOCS, x => x === -1 || x === 0 || x === 1);
 
-exports.$Future = BinaryType(F.name, F_DOCS, F.isFuture, F.extractLeft, F.extractRight);
+E.$Future = BinaryType(F.name, F_DOCS, F.isFuture, F.extractLeft, F.extractRight);
 
-exports.$ErrorLike = NullaryType('ErrorLike', '', x =>
+E.$ErrorLike = NullaryType('ErrorLike', '', x =>
   typeof x === 'string'
   || x instanceof Error
-  || is(exports.$Error, x)
+  || is(E.$Error, x)
 );
 
-exports.$Buffer = NullaryType('Buffer', '', x => x instanceof Buffer);
+E.$Buffer = NullaryType('Buffer', '', x => x instanceof Buffer);
 
-exports.env = env.concat([
-  exports.$Future($.Unknown, $.Unknown),
-  exports.$Pair($.Unknown, $.Unknown),
-  exports.$Buffer,
-  exports.$ErrorLike
+E.env = env.concat([
+  E.$Future($.Unknown, $.Unknown),
+  E.$Pair($.Unknown, $.Unknown),
+  E.$Buffer,
+  E.$ErrorLike
 ]);
