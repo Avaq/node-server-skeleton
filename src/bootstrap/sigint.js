1c4ab81e7c1544de88a548a1e5f4b98236c39a9f
Aldwin Vlasblom <aldwin.vlasblom@gmail.com>
2017-06-02 18:02:11 +0200

    Only send process messages if it's available

--- src/bootstrap/sigint.js
+++ src/bootstrap/sigint.js
@@ -8,7 +8,7 @@ const {K} = require('../prelude');
 //This bootstrapper keeps the process alive until a SIGINT is received.
 //     default :: a -> Middleware a b ()
 module.exports = K(Middleware.lift(Future((rej, res) => {
-  process.send('ready');
+  typeof process.send === 'function' && process.send('ready');
   log.info('Ready for take-off\n');
   process.once('SIGINT', _ => {
     process.removeAllListeners('uncaughtException');
